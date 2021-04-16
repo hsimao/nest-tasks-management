3e73d8cd2a339151cdfb5ae3acb8f6f0ebd00354
hsimao <e087754958@gmail.com>
2021-04-16 15:50:05 +0800

    feat: getTasks add validator

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TASK_STATUS } from '../task.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn(Object.values(TASK_STATUS))
   status: TASK_STATUS;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
