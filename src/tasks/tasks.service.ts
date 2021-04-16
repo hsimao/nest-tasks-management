71e28ae53c6fcc6a9b976bf17774e99a99123219
hsimao <e087754958@gmail.com>
2021-04-16 14:39:32 +0800

    feat: deleteTask add validator

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -55,8 +55,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    console.log(id);
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TASK_STATUS): Task {
